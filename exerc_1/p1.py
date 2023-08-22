0f2afd72e5366a62a5edc218c5cc68a805aafc28
Alisson Michel Sganzerla <alisson.sganzerla@acad.ufsm.br>
2023-08-22 11:05:39 -0300

    chore: p1

--- exerc_1/p1.py
+++ exerc_1/p1.py
@@ -1,38 +1,73 @@
 from ortools.linear_solver import pywraplp
+# python -m pip install ortools
 class InvestProbl:
     def __init__(self):
-        # Criar o modelo linear
-        mdl = pywraplp.Solver.CreateSolver("GLOP")
-        
+       
+       # a) deve-se escolher a melhor combinação de títulos para investir de modo que obtenha-se o máximo lucro possível.
+       
         # dados
-        investimentos = [i for i in range(1, 7)]
-        risco = {1:1, 2:3, 3:4, 4:2, 5:4, 6:5}
-        vencimento = {1:15, 2:12, 3:8, 4:7, 5:11, 6:5}
-        retorno = {1:0.087, 2:0.095, 3:0.12, 4:0.09, 5:0.13, 6:0.2}
+        self.investimentos = [i for i in range(1, 7)]
+        self.risco = {1:1, 2:3, 3:4, 4:2, 5:4, 6:5}
+        self.vencimento = {1:15, 2:12, 3:8, 4:7, 5:11, 6:5}
+        self.retorno = {1:0.087, 2:0.095, 3:0.12, 4:0.09, 5:0.13, 6:0.2}
         
-        # variáveis
-        x = {i: mdl.NumVar(name=f'x_{i}', lb=0, ub=mdl.infinity()) for i in investimentos}
-
-        # objetivo
-        mdl.Maximize(mdl.Sum([x[i] * retorno[i] for i in investimentos]))
-
-        # restrição  1
-        for i in investimentos:
-            mdl.Add(x[i] <= 0.25)
+        # Criar o modelo linear
+        self.mdl = pywraplp.Solver.CreateSolver("GLOP")
         
-        # restrição  2
-        mdl.Add(mdl.Sum(x[i] for i in investimentos if vencimento[i] > 10) >= 0.51 )
-  
-        # restrição  3
-        mdl.Add(mdl.Sum(x[i] for i in investimentos if risco[i] >= 4) <= 0.50 )
+        # variaveis
+        self.criando_variaveis()
 
-        status = mdl.Solve()
+        # restricoes
+        self.criando_restricoes()
+        
+        # funcao objetivo
+        self.criando_funcao_objetivo()
+
+        # resolver e apresentar solução
+        self.resolver_problema()
+
+    def resolver_problema(self):
+        status = self.mdl.Solve()
         
         if status == pywraplp.Solver.OPTIMAL:
-            print(f"O lucro máximo é: {mdl.Objective().Value():.2f}")
-            for i in investimentos:
+            print(f"O lucro máximo é: {self.mdl.Objective().Value():.2f}")
+            for i in self.investimentos:
                 # if x[i].solution_value() > 0:
-                    print(f"Investimento {i}, de risco {risco[i]}, com retorno de {(retorno[i] * 100):.2f}% em {vencimento[i]} anos. Investir {(x[i].solution_value() * 100)}%.")
+                    print(f"Investimento {i}, de risco {self.risco[i]}, com retorno de {(self.retorno[i] * 100):.2f}% em {self.vencimento[i]} anos. Investir {(self.x[i].solution_value() * 100):.2f}%.")
+        else:
+            print(f"Problema não resolvido: {status}")
+            
+            
+    def criando_funcao_objetivo(self):
+        # d) cada variavel x tem o seu respectivo coeficiente de retorno contribuindo para o peso da funcao objetivo
+        self.mdl.Maximize(self.mdl.Sum([self.x[i] * self.retorno[i] for i in self.investimentos]))
+
+
+    def criando_restricoes(self):
+
+        # restrição  1
+        # nenhum título deve possuir mais de 25% do total investido
+        for i in self.investimentos:
+            self.mdl.Add(self.x[i] <= 0.25)
+
+        # restrição  2
+        # 50% do total investido deve ser aplicado no título 1, 2 e 6
+        self.mdl.Add(self.mdl.Sum(self.x[i] for i in self.investimentos if self.vencimento[i] > 10) >= 0.51)
+        
+        
+        # restrição  3
+        # Os títulos 3, 5  e 6 não devem representar mais de 50% do valor investido.
+        self.mdl.Add(self.mdl.Sum(self.x[i] for i in self.investimentos if self.risco[i] >= 4) <= 0.50)
+
+        # restrição 4
+        # soma de todos os investimentos deve ser 100%
+        self.mdl.Add(self.mdl.Sum(self.x[i] for i in self.investimentos) == 1)
+        
+
+
+    def criando_variaveis(self):
+        # c) x vai representar o percentual que deve ser investido em cada investimento
+        self.x = {i: self.mdl.NumVar(name=f'x_{i}', lb=0, ub=self.mdl.infinity()) for i in self.investimentos}
 
 if __name__ == "__main__":
     
