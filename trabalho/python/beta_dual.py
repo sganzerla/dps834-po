74ef97e4d8f6b3f7e24251502d96a0fe434f6b7f
Alisson Michel Sganzerla <alisson.sganzerla@acad.ufsm.br>
2023-10-25 09:01:58 -0300

    chore: trabalho prático

---
+++ trabalho/python/beta_dual.py
@@ -0,0 +1,57 @@
+from ortools.linear_solver import pywraplp
+# python -m pip install ortools
+
+class BetaD:
+    def __init__(self):
+        
+        
+        # Criar o modelo linear
+        self.mdl = pywraplp.Solver.CreateSolver("GLOP")
+        self.var()
+        self.fo()
+        self.const()
+            
+       
+    def var(self):
+        # fabricar 1000 auto no total
+        self.y1 = self.mdl.NumVar(name=f'y_1', lb=0, ub=self.mdl.infinity())
+        # fabricar 400 em vitoria
+        self.y2 = self.mdl.NumVar(name=f'y_2', lb=0, ub=self.mdl.infinity())
+        # mao de obra
+        self.y3 = self.mdl.NumVar(name=f'y_3', lb=0, ub=self.mdl.infinity())
+        # materia prima
+        self.y4 = self.mdl.NumVar(name=f'y_4', lb=0, ub=self.mdl.infinity())
+
+    def fo(self):
+            self.mdl.Maximize(1000 * self.y1 + 400 * self.y2 - 3300 * self.y3 - 4000 * self.y4)
+        
+        
+    def const(self):
+        
+            self.mdl.Add(1 * self.y1 + 0 * self.y2 - 2 * self.y3 - 3 * self.y4 <= 15)
+            
+            self.mdl.Add(1 * self.y1 + 0 * self.y2 - 3 * self.y3 - 4 * self.y4 <= 10)
+            
+            self.mdl.Add(1 * self.y1 + 1 * self.y2 - 4 * self.y3 - 5 * self.y4 <= 9)
+            
+            self.mdl.Add(1 * self.y1 + 0 * self.y2 - 4 * self.y3 - 6 * self.y4 <= 7)
+
+
+    def solve(self):
+   
+            status = self.mdl.Solve()
+            
+            if status == pywraplp.Solver.OPTIMAL:
+                print(f"FO: {self.mdl.Objective().Value():.2f}")
+                print(f'Cada carro produzido aumenta a FO em y_1 = {self.y1.solution_value():.2f}')
+                print(f'Cada carro produzido em Vitória aumenta a FO y_2 = {self.y2.solution_value():.2f}')
+                print(f'y_3 = {self.y3.solution_value():.2f}')
+                print(f'Cada carro produzido utiliza mão de obra de y_4 = {self.y4.solution_value():.2f}')
+            else:
+                print(f"Problema não resolvido: {status}")
+
+if __name__ == "__main__":
+    
+    model = BetaD()
+
+    model.solve()
---
+++ trabalho/python/beta_primal.py
@@ -0,0 +1,75 @@
+from ortools.linear_solver import pywraplp
+# python -m pip install ortools
+
+class BetaP:
+    def __init__(self):
+        
+        
+        
+        # Criar o modelo linear
+        self.mdl = pywraplp.Solver.CreateSolver("GLOP")
+        self.var()
+        self.fo()
+        self.const()
+            
+       
+    def var(self):
+        # Rio de Janeiro
+        self.x1 = self.mdl.NumVar(name=f'x_1', lb=0, ub=self.mdl.infinity())
+        # São Paulo
+        self.x2 = self.mdl.NumVar(name=f'x_2', lb=0, ub=self.mdl.infinity())
+        # Vitória
+        self.x3 = self.mdl.NumVar(name=f'x_3', lb=0, ub=self.mdl.infinity())
+        # Uberaba
+        self.x4 = self.mdl.NumVar(name=f'x_4', lb=0, ub=self.mdl.infinity())
+
+    def fo(self):
+            self.mdl.Minimize(15 * self.x1 + 10 * self.x2 + 9 * self.x3 + 7 * self.x4)
+        
+        
+        # 15 +10 + 9 + 7
+        #  1 + 1 + 1 + 1 == 1000
+        #  0 + 0 + 1 + 0 >= 400
+        #  2 + 3 + 4 + 4 <= 3300
+        #  3 + 4 + 5 + 6 <= 4000
+        
+        # colocando na forma padrão
+        #15 + 10 + 9 + 7
+        #  1 + 1 + 1 + 1 == 1000
+        #  0 + 0 + 1 + 0 >= 400
+        # -2 - 3 - 4 - 4 >= -3300
+        # -3 - 4 - 5 - 6 >= -4000
+        
+        
+    def const(self):
+        
+            # deve construir 1000 automoveis
+            self.mdl.Add(1 * self.x1 + 1 * self.x2 + 1 * self.x3 + 1 * self.x4 == 1000)
+
+            # fabrica de vitoria 400 carros
+            self.mdl.Add(0 * self.x1 + 0 * self.x2 + 1 * self.x3 + 0 * self.x4 >= 400)
+            
+            # mao de obra disponivel
+            self.mdl.Add(2 * self.x1 + 3 * self.x2 + 4 * self.x3 + 5 * self.x4 <= 3300)
+
+            # materia prima
+            self.mdl.Add(3 * self.x1 + 4 * self.x2 + 5 * self.x3 + 6 * self.x4 <= 4000)            
+
+    def solve(self):
+   
+            status = self.mdl.Solve()
+            
+            if status == pywraplp.Solver.OPTIMAL:
+                print(f"FO: {self.mdl.Objective().Value():.2f}")
+                print(f'Produção de carros na fábrica x_1 = {self.x1.solution_value():.2f}')
+                print(f'Produção de carros na fábrica x_2 = {self.x2.solution_value():.2f}')
+                print(f'Produção de carros na fábrica x_3 = {self.x3.solution_value():.2f}')
+                print(f'Produção de carros na fábrica x_4 = {self.x4.solution_value():.2f}')
+            else:
+                print(f"Problema não resolvido: {status}")
+
+if __name__ == "__main__":
+    
+    model = BetaP()
+
+    model.solve()
